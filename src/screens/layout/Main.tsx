9a87098774c4c6f33e4d691ad00e7c5410b06cea
GaryGolf <garygolf@mail.ru>
2023-09-14 14:45:09 +0300

    feat: add layout screen component

--- src/screens/layout/Main.tsx
+++ src/screens/layout/Main.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { observer } from "mobx-react-lite";
-import './App.css';
-import { useStore } from "./store";
+import styles from './layout.module.css';
+import { useStore } from "../../store";
 
-import { PersonalForm, personalForm as form } from "./forms/personal";
+import { PersonalForm, personalForm as form } from "../../forms/personal";
 
 
-function App() {
+function Main() {
   const isLoading = useStore(store => store.personal.isLoading);
   
   return (
-    <div className="App">
+    <div className={styles.container}>
       { isLoading && <p>Loading...</p> }
       <PersonalForm form={form}/>
     </div>
   );
 }
 
-export default observer(App);
+export default observer(Main);
